41226f209dcbe604136ad42d4737861b87934d40
Stephen Kelly <stevekm@users.noreply.github.com>
2017-08-22 15:38:23 -0400

    first commit

---
+++ config/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Configurations module
+
+Developed and tested with Python 2.7
+'''
+# ~~~~~ LOGGING ~~~~~~ #
+import logging
+logger = logging.getLogger("config")
+logger.debug("loading config module")
+
+# ~~~~~ SETUP ~~~~~~ #
+import yaml
+import os
+
+scriptdir = os.path.dirname(os.path.realpath(__file__))
+
+logger.debug("loading configurations...")
+
+with open(os.path.join(scriptdir, 'Delly2.yml'), "r") as f:
+    Delly2 = yaml.load(f)
+
+logger.debug(sns)
+logger.debug(misc)
+logger.debug(NextSeq)
+logger.debug(Delly2)
